051e8802bd69454709c7c1dec68703bc56bd5f7b
PoHung Wang <wang.po.hung@gmail.com>
2021-06-06 19:12:23 -0700

    finishing sign in handleSummit method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import FromInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-import { signInWithGoogle } from "../../firebase/firebase.util";
+import { auth, signInWithGoogle } from "../../firebase/firebase.util";
 import "./sign-in.styles.scss";
 
 class SignIn extends React.Component {
@@ -14,9 +14,15 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSumit = (event) => {
+  handleSumit = async (event) => {
     event.preventDefault();
-    this.setState({ email: "", password: "" });
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.log(error);
+    }
   };
   handleChange = (event) => {
     const { value, name } = event.target;
