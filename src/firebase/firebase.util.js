d8d8353a61d077c32584bb45c21de2297dff8091
PoHung Wang <wang.po.hung@gmail.com>
2021-06-06 19:04:38 -0700

    implemented firebase utils, allow user to sign up with emails

--- src/firebase/firebase.util.js
+++ src/firebase/firebase.util.js
@@ -11,8 +11,32 @@ const config = {
   appId: "1:548933909230:web:f3feeea838f240686651ff",
   measurementId: "G-MYTHPWWK02",
 };
-
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
@@ -20,5 +44,4 @@ export const firestore = firebase.firestore();
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
-
 export default firebase;
